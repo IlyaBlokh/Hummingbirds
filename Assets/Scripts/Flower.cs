cc80fe44e4bb989ca0a5fe484f314037aa6d09ac
ilyablokh <ilyablokh@gmail.com>
2023-05-25 16:16:20 +0300

    initialize Flower

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -1,14 +1,15 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class Flower : MonoBehaviour
 {
     private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
-    private Collider flowerCollider;
     private Material flowerMaterial;
     
-    [HideInInspector]
+    [SerializeField] private Collider flowerCollider;
+    
     public Collider nectarCollider;
-
     public Color filledColor = new Color(1f, 0, 0.3f);
     public Color emptyColor = new Color(0.5f, 0, 1f);
 
@@ -17,6 +18,20 @@ public class Flower : MonoBehaviour
     public float NectarAmount { get; private set; }
     public bool HasNectar => NectarAmount > 0f;
 
+    private void Awake()
+    {
+        flowerMaterial = GetComponent<MeshRenderer>().material;
+        SetDefault();
+    }
+
+    public void SetDefault()
+    {
+        NectarAmount = 1f;
+        flowerCollider.enabled = true;
+        nectarCollider.enabled = true;
+        flowerMaterial.SetColor(BaseColor, filledColor);
+    }
+    
     public float Feed(float amount)
     {
         float amountToTake = Mathf.Clamp(amount, 0f, NectarAmount);
