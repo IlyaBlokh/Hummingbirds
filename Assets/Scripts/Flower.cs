55bc13935d9486affad2b4fcd8fb79b0a3cdcb09
ilyablokh <ilyablokh@gmail.com>
2023-05-25 16:04:34 +0300

    add Flower component, add properties and Feed method

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+    private Collider flowerCollider;
+    private Material flowerMaterial;
+    
+    [HideInInspector]
+    public Collider nectarCollider;
 
-    // Update is called once per frame
-    void Update()
+    public Color filledColor = new Color(1f, 0, 0.3f);
+    public Color emptyColor = new Color(0.5f, 0, 1f);
+
+    public Vector3 FlowerUpVector => nectarCollider.transform.up;
+    public Vector3 FlowerCenterPosition => nectarCollider.transform.position;
+    public float NectarAmount { get; private set; }
+    public bool HasNectar => NectarAmount > 0f;
+
+    public float Feed(float amount)
     {
-        
+        float amountToTake = Mathf.Clamp(amount, 0f, NectarAmount);
+        NectarAmount -= amountToTake;
+        if (!HasNectar)
+        {
+            flowerCollider.enabled = false;
+            nectarCollider.enabled = false;
+            flowerMaterial.SetColor(BaseColor, emptyColor);
+        }
+
+        return amountToTake;
     }
 }
